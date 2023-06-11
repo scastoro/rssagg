188460c9d45bc629285434230b2a76d1c69a8336
Sal Castoro <salcastoro@gmail.com>
2023-06-11 10:26:18 -0400

    Add vendor & error handler

--- json.go
+++ json.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error:", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, errResponse{
+		Error: msg,
+	})
+}
+
 func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
